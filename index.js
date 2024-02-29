e2e873aad850736f579fecd79fd10ed9a0e0d113
ChocolatePie1 <152132714+ChocolatePie1@users.noreply.github.com>
2024-02-29 08:49:57 +0800

    1234
    4321

---
+++ index.js
@@ -0,0 +1,10 @@
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  <main>
+<div className="bg-red-500 h-96 w-screen "> </div>
+  </main>
+}
